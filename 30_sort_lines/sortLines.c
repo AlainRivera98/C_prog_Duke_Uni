18a41f91c996978481bb4fe427c3e24152615274
Coursera Learner <nobody@nowhere.nul>
2021-01-31 05:38:44 +0000

    Did assignment 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,9 +16,60 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void printData(char ** array, size_t n){
+  for(size_t i = 0; i < n; i++){
+    printf("%s",array[i]);
+    free(array[i]);
+  }
+}
+
+void sortFile(FILE *f){
+  char ** array = NULL; //array to store strings
+  char * line = NULL; //current string
+  size_t sz; //getline will initialize it
+  size_t i = 0; //store number of strings
+  while(getline(&line,&sz,f) >= 0){
+    array = realloc(array,(i+1)*sizeof(*array));
+    array[i] = line;
+    line = NULL;
+    i++;
+  }
+
+  free(line);
+  sortData(array, i);
+  printData(array, i);
+  free(array);
+}
+
 int main(int argc, char ** argv) {
   
-  //WRITE YOUR CODE HERE!
+  if(argc <= 0){
+    perror("Strange mistake found");
+    return EXIT_FAILURE;
+
+  } else if (argc == 1){
+    //Read from standart input and sort
+    sortFile(stdin);
+
+  } else {
+    //Read all inputs and sort them
+    for(int i = 1; i < argc; i++){
+      FILE * f = fopen(argv[i],"r");
+      
+      if (f == NULL){
+	perror("Could not open file");
+	return EXIT_FAILURE;
+      }
+
+      //print the sorted file
+      sortFile(f);
+
+      if (fclose(f) != 0){
+	perror("Failed to close file number %d,i");
+	return EXIT_FAILURE;
+      }
+    }
+  }
   
   return EXIT_SUCCESS;
 }
