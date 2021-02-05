383cb7f4dc9718eca0d72fde6a396ab61b5932ab
Coursera Learner <nobody@nowhere.nul>
2021-02-05 06:03:29 +0000

    Did assignment 34

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -5,34 +5,74 @@
 #include "counts.h"
 #include "outname.h"
 
+void fillCounts(counts_t * c, kvarray_t * kv, FILE * f){
+  char * line = NULL;
+  size_t sz;
+
+  while(getline(&line, &sz, f) >= 0){
+    char * p = strchr(line,'\n');
+    if(p != NULL){
+      *p = '\0';
+    }
+    char * value = lookupValue(kv,line);
+    addCount(c,value);
+  }
+
+  free(line);
+}
+
+
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
-  //WRITE ME
-  return NULL;
+  counts_t * c = createCounts();
+
+  FILE * f = fopen(filename,"r");
+
+  if(f == NULL){
+    perror("Could not open file");
+    exit(EXIT_FAILURE);
+  }
+
+  fillCounts(c, kvPairs, f);
+
+  if(fclose(f) != 0){
+    perror("Failed to close the file");
+    exit(EXIT_FAILURE);
+  }
+  
+  return c;
 }
 
 int main(int argc, char ** argv) {
-  //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
+  //Check args errors
+  if(argc <= 2){
+    perror("Need at least 2 files as arguments\n"); 
+    return EXIT_FAILURE;
+  }
 
- //count from 2 to argc (call the number you count i)
+  //read key/value pairs from first file
+  kvarray_t * kv = readKVs(argv[1]);
+  
+  for(int i = 2; i < argc; i++){
+    counts_t * c = countFile(argv[i], kv);
+    char * outName = computeOutputFileName(argv[i]);
 
-    //count the values that appear in the file named by argv[i], using kv as the key/value pair
-    //   (call this result c)
+    FILE * f = fopen(outName, "w");
+    if (f == NULL){
+      perror("Could not creat file %s\n");
+      return EXIT_FAILURE;
+    }
 
-    //compute the output file name from argv[i] (call this outName)
+    printCounts(c,f);
 
+    if(fclose(f) != 0){
+      return EXIT_FAILURE;
+    }
 
-    //open the file named by outName (call that f)
+    free(outName);
+    freeCounts(c);
+  }
 
-    //print the counts from c into the FILE f
-
-    //close f
-
-    //free the memory for outName and c
-
-
-
- //free the memory for kv
+  freeKVs(kv);
 
   return EXIT_SUCCESS;
 }
