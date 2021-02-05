3f29cdf6edbe44fa1ee2b0a73552576e7a401d5e
Coursera Learner <nobody@nowhere.nul>
2021-02-05 02:30:24 +0000

    Did assignment 33

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -2,16 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 #include "counts.h"
+
 counts_t * createCounts(void) {
-  //WRITE ME
+  //Initialize counts_t c and its elements
+  counts_t * c = malloc(sizeof(counts_t));
+  c->array = NULL;
+  c->n = 0;
+  c->unknown_count = 0;
+
+  return c;
 }
+
+one_count_t * newValue(const char * name){
+  one_count_t * newCount = malloc(sizeof(one_count_t));
+  
+  newCount->value = malloc((strlen(name) + 1)*sizeof(char));
+  strncpy(newCount->value,name, strlen(name) + 1);
+  newCount->count = 1;
+
+  return newCount;
+}
+
 void addCount(counts_t * c, const char * name) {
-  //WRITE ME
+  if(name != NULL){
+    for(size_t i = 0; i < c->n; i++){
+      if(strcmp(name,c->array[i]->value) == 0){
+	c->array[i]->count++;
+	return;
+      }
+    }
+
+    //If new name/value
+    //*c->array = *one_count_t
+    c->array = realloc(c->array,(c->n + 1)*sizeof(*c->array));
+    c->array[c->n] = newValue(name);
+    c->n++;
+    
+  } else {
+    c->unknown_count++;
+  }
 }
+
 void printCounts(counts_t * c, FILE * outFile) {
-  //WRITE ME
+  if(outFile == NULL){
+    perror("Problem with outfile");
+    exit(EXIT_FAILURE);
+  }
+
+  for(size_t i = 0; i < c->n; i++){
+    fprintf(outFile, "%s: %lu\n", c->array[i]->value, c->array[i]->count);
+  }
+
+  if(c->unknown_count){
+    fprintf(outFile, "<unknown> : %lu\n", c->unknown_count);
+  }
 }
 
 void freeCounts(counts_t * c) {
-  //WRITE ME
+  for(size_t i = 0; i < c->n; i++){
+    free(c->array[i]->value);
+    free(c->array[i]);
+  }
+
+  free(c->array);
+  free(c);
 }
